e0218e36f80b4aa0dcf4a735cbd1aaa070d48b4a
samueltrevor <shmueltrevor@gmail.com>
2022-09-28 01:53:31 +0300

    React Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import react, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent'
 import './App.css';
 
@@ -6,9 +7,11 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Main/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
