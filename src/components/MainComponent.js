6aa10d8146c57140f45fd4398da07229a0734004
samueltrevor <shmueltrevor@gmail.com>
2022-11-05 21:44:30 +0200

    assignmeant 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,9 @@ import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Footer from './FooterComponent';
+import About from './AboutComponent.js'
 
+  
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -22,12 +24,18 @@ class Main extends Component {
         };
     }
 
+    
     onDishSelect(dishId) {
         this.setState({ selectedDish: dishId })
     }
 
     render() {
-
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            )
+          }
         const HomePage = () => {
             return (
                 <Home
@@ -44,6 +52,8 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
